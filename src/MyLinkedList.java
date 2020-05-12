80188f2bc8c458291a8f585c9fef8a25c561f0ca
Kira <datdhvinh@gmail.com>
2020-05-12 22:01:53 +0700

    xây dựng lại lớp linked list

---
+++ src/MyLinkedList.java
@@ -0,0 +1,54 @@
+public class MyLinkedList<E> {
+    private Node head;
+    private int numNodes = 0;
+
+    public MyLinkedList(Object data) {
+        head = new Node(data);
+    }
+
+    public class Node {
+        private Node next;
+        private Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData() {
+            return this.data;
+        }
+    }
+
+    public void add(int index, E element) {
+        Node temp = head;
+        Node holder;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        holder = temp;
+        temp.next = new Node(element);
+        temp.next.next = holder;
+        numNodes++;
+    }
+
+    public void addFirst(E element) {
+        Node temp = head;
+        head = new Node(element);
+        head.next = temp;
+        numNodes++;
+    }
+
+    public void addLast(E element) {
+        Node temp = head;
+        Node holder;
+        for (int i=0;i<numNodes;i++){
+            temp=temp.next;
+        }
+        temp.next=new Node(element);
+        numNodes++;
+    }
+
+//    public E remove(int index){
+//        if ()
+//    }
+}
---
+++ src/MyLinkedListTest.java
@@ -0,0 +1,7 @@
+import java.util.LinkedList;
+
+public class MyLinkedListTest {
+    public static void main(String[] args) {
+        LinkedList
+    }
+}
